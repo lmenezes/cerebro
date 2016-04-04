c98572c000e30da111c13e6e4aeee70aa2335a5e
Leonardo Menezes <leonardo.menezes@xing.com>
2016-04-04 13:59:59 +0200

    added test to GetIndexMappingController

--- app/controllers/GetIndexMappingController.scala
+++ app/controllers/GetIndexMappingController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-class GetIndexMapping extends ElasticActionController {
+class GetIndexMappingController extends ElasticActionController {
 
   def processElasticRequest = (request, client) => client.getIndexMapping(request.get("index"), request.host)
 
---
+++ test/controllers/GetIndexMappingControllerSpec.scala
@@ -0,0 +1,51 @@
+package controllers
+
+import elastic.{ElasticResponse, ElasticClient}
+import exceptions.MissingRequiredParamException
+import models.CerebroRequest
+import org.specs2.Specification
+import org.specs2.mock.Mockito
+import play.api.libs.json.Json
+import play.api.test.FakeApplication
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
+object GetIndexMappingControllerSpec extends Specification with Mockito {
+
+  def is =
+    s2"""
+    GetIndexMappingController should                   ${step(play.api.Play.start(FakeApplication()))}
+      invoke getIndexMapping                           $getIndexMapping
+      should throw exception if index param is missing $missingIndex
+                                                       ${step(play.api.Play.stop(FakeApplication()))}
+      """
+
+  val controller = new GetIndexMappingController
+
+  def getIndexMapping = {
+    val expectedResponse = Json.parse(
+      """
+        |{
+        |  "someIndex": {
+        |    "mappings": {}
+        |  }
+        |}
+      """.stripMargin
+    )
+    val body = Json.obj("host" -> "somehost", "index" -> "someIndex")
+    val client = mock[ElasticClient]
+    client.getIndexMapping("someIndex", "somehost") returns Future.successful(ElasticResponse(200, expectedResponse))
+    val response = Await.result(controller.processElasticRequest(CerebroRequest(body), client), Duration("1s"))
+    there was one(client).getIndexMapping("someIndex", "somehost")
+    response.body mustEqual expectedResponse
+    response.status mustEqual 200
+  }
+
+  def missingIndex = {
+    val body = Json.obj("host" -> "somehost")
+    val client = mock[ElasticClient]
+    controller.processElasticRequest(CerebroRequest(body), client) must throwA[MissingRequiredParamException]
+  }
+
+}
