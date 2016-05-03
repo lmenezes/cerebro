ce716d6da768d5a6fb4a3b6b8c4e2714ba2fa915
Leonardo Menezes <leonardo.menezes@xing.com>
2016-05-03 16:23:15 +0200

    fix shard replica styling

--- public/app.js
+++ public/app.js
@@ -657,22 +657,6 @@ angular.module('cerebro').directive('ngProgress',
     }
 );
 
-angular.module('cerebro').directive('ngShard',
-    function () {
-
-      return {
-        scope: {
-          shard: '=shard'
-        },
-        template: function (elem, attrs) {
-          return '<span class="shard shard-{{shard.state.toLowerCase()}}" ng-class="{\'shard-replica\': !shard.primary && shard.node}">' +
-              '<small>{{shard.shard}}</small>' +
-              '</span>';
-        }
-      };
-    }
-);
-
 angular.module('cerebro').filter('bytes', function() {
 
     var UNITS = ['b', 'KB', 'MB', 'GB', 'TB', 'PB'];
