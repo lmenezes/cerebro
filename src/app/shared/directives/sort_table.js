c70642b9e3281da4030352e9a93666b9cbec852f
Leonardo Menezes <leonardo.menezes@xing.com>
2017-03-09 19:48:27 +0100

    added sort table directive

---
+++ src/app/shared/directives/sort_table.js
@@ -0,0 +1,45 @@
+angular.module('cerebro').directive('ngSortBy',
+  function() {
+
+    function updateSortingIcon(scope, elem, attrs) {
+      var sorts = scope.sortBy === attrs.property;
+      var sortIcon = elem.find('i');
+      sortIcon.removeClass('fa-sort-asc fa-sort-desc');
+      if (sorts) {
+        if (scope.reverse) {
+          sortIcon.addClass('fa-sort-desc');
+        } else {
+          sortIcon.addClass('fa-sort-asc');
+        }
+      }
+    }
+
+    function link(scope, elem, attrs) {
+      scope.$watch(
+        function() {
+          return scope.sortBy;
+        },
+        function() {
+          updateSortingIcon(scope, elem, attrs);
+        });
+
+      scope.$watch(
+        function() {
+          return scope.reverse;
+        },
+        function() {
+          updateSortingIcon(scope, elem, attrs);
+        }
+      );
+    }
+
+    return {
+      link: link,
+      template: function(elem, attrs) {
+        return '<a href="" target="_self" ng-click=setSortBy(\'' +
+          attrs.property + '\')>' + attrs.text +
+          '<i class="fa fa-fw fa-sort-asc"></i></a>';
+      }
+    };
+  }
+);
