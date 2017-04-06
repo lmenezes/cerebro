d5065d775781122a7bc2b2242a831033056d1fde
Leonardo Menezes <leonardo.menezes@xing.com>
2017-04-06 18:44:45 +0200

    consider both string and text types as analyse-able
    closes #115

--- app/models/analysis/IndexFields.scala
+++ app/models/analysis/IndexFields.scala
@@ -4,6 +4,14 @@ import play.api.libs.json._
 
 object IndexFields {
 
+  private final val AnalyzableFields = Seq(
+    "string", // FIXME: ES 2.X
+    "text"
+  )
+
+  private final def analyzable(fieldType: String) =
+    AnalyzableFields.contains(fieldType)
+
   def apply(index: String, data: JsValue) = {
     val docTypes = (data \ index \ "mappings").as[JsObject].keys
     val fields = docTypes.flatMap { docType =>
@@ -21,16 +29,16 @@ object IndexFields {
 
           case p if (data \ p \ "fields").asOpt[JsObject].isDefined =>
             val fields = (data \ p \ "fields").as[JsObject].keys.collect {
-              case field if (data \ p \ "fields" \ field \ "type").asOpt[String].exists(_.equals("string")) =>
+              case field if (data \ p \ "fields" \ field \ "type").asOpt[String].exists(analyzable) =>
                 s"$p.$field"
             }.toSeq
-            if ((data \ p \ "type").asOpt[String].exists(_.equals("string"))) {
+            if ((data \ p \ "type").asOpt[String].exists(analyzable)) {
               fields :+ p
             } else {
               fields
             }
 
-          case p if (data \ p \ "type").asOpt[String].exists(_.equals("string")) =>
+          case p if (data \ p \ "type").asOpt[String].exists(analyzable) =>
             Seq(p)
         }.flatten.toSeq
       case _ => Seq()
