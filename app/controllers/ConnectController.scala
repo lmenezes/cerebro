7875836d7fe59099ae7fb77843ad6bb49ed5f0dd
Leonardo Menezes <mail@lmenezes.com>
2017-10-25 19:53:26 +0200

    created connect api to check connectivity with a cluster

--- app/controllers/ConnectController.scala
+++ app/controllers/ConnectController.scala
@@ -3,16 +3,26 @@ package controllers
 import javax.inject.Inject
 
 import controllers.auth.AuthenticationModule
-import models.{CerebroResponse, Hosts}
+import elastic.ElasticClient
+import models.{CerebroRequest, CerebroResponse, Hosts}
 import play.api.libs.json.{JsArray, JsString}
 import play.api.mvc.Controller
 
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ConnectController @Inject()(val authentication: AuthenticationModule,
+                                  elastic: ElasticClient,
                                   hosts: Hosts) extends Controller with AuthSupport {
 
-  def index = AuthAction(authentication) { request =>
+  def index = AuthAction(authentication) { _ =>
     CerebroResponse(200, JsArray(hosts.getHostNames().map(JsString(_))))
+  }
+
+  def connect = AuthAction(authentication).async(parse.json) { request =>
+    val req = CerebroRequest(request, hosts)
+    elastic.executeRequest("GET", "_cluster/health", None, req.target).map {
+      response => CerebroResponse(response.status, response.body)
+    }
   }
 
 }
