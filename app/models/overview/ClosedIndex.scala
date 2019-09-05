9d8b029deac69cd6edfa2cf574f7aaba36569d70
Rassyan <yjkhngds@gmail.com>
2019-09-05 12:08:27 +0800

    closed indices also show its aliases and number of shards and replicas

--- app/models/overview/ClosedIndex.scala
+++ app/models/overview/ClosedIndex.scala
@@ -1,14 +1,17 @@
 package models.overview
 
-import play.api.libs.json.{JsBoolean, JsString, Json}
+import play.api.libs.json.{JsBoolean, JsNumber, JsString, JsValue, Json}
 
 object ClosedIndex {
 
-  def apply(name: String) =
+  def apply(name: String, aliases: JsValue, numShards: JsNumber, numReplicas: JsNumber) =
     Json.obj(
       "name" -> JsString(name),
       "closed" -> JsBoolean(true),
-      "special" -> JsBoolean(name.startsWith("."))
+      "special" -> JsBoolean(name.startsWith(".")),
+      "aliases" -> aliases,
+      "num_shards" -> numShards,
+      "num_replicas" -> numReplicas
     )
 
 }
