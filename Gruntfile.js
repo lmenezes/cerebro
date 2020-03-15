327f0bb8f85fcff29aa2fc400572e1324081cfda
H. Habighorst <h.habighorst@protonmail.com>
2020-03-15 19:04:44 +0100

    Migrated Gruntfile.
    - pick up a less compiler, to generate bootstrap from source
    - pick up a css minifier, to reduce the CSS footage
    - copy the necessary fonts (FontAwesome / Glyphicons from bootstrap)
    - copy the necessary ACE theme file
    - replaced JSCS with ESLint
    - concat angular from the minified sources
    - concat ACE from the minified sources
    - copy the necessary ACE files for JSON worker and parser
    - copy JQuery from the minified sources
    
    I utilize a directory src/assets/temp for the bootstrap CSS generation to not pollute the root folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,8 @@
 module.exports = function(grunt) {
-
   grunt.initConfig({
     clean: {
       dist: {
-        src: [
-          'public/css',
-          'public/js'
-        ]
+        src: [ 'public/css', 'public/js', 'src/temp', 'public/fonts' ]
       }
     },
     watch: {
@@ -19,30 +15,32 @@ module.exports = function(grunt) {
       }
     },
     copy: {
-      main: {
-        files: []
-      }
+      font_awesome: {expand: true, cwd: 'node_modules/font-awesome/fonts/', src: '*', dest: 'public/fonts/'},
+      font_bs_glyphicons: {expand: true, cwd: 'node_modules/bootstrap/fonts/', src: '*', dest: 'public/fonts/'},
+      ace_theme: {expand: true, cwd: 'src/assets/js/ace', src: '*', dest: 'public/js/ace'},
+      ace_scripts: {expand: true, cwd: 'node_modules/ace-builds/src-min/', src: ['mode-json.js', 'worker-json.js'], dest: 'public/js/ace'}
+    },
+    less: {
+        bootstrap: {
+            files: {
+                'src/assets/temp/bootstrap.css': 'src/assets/less/bootstrap-build.less'
+            }
+        },
     },
     concat: {
       vendorjs: {
         src: [
-          'src/assets/libs/jquery/*.js',
-          'src/assets/libs/angularjs/angular.min.js',
-          'src/assets/libs/angularjs/angular-animate.min.js',
-          'src/assets/libs/angularjs/angular-route.min.js',
-          'src/assets/libs/bootstrap/bootstrap.min.js',
+          'node_modules/jquery/dist/jquery.min.js',
+          'node_modules/angular/angular.min.js',
+          'node_modules/angular-animate/angular-animate.min.js',
+          'node_modules/angular-route/angular-route.min.js',
+          'node_modules/bootstrap/dist/js/bootstrap.min.js',
           'src/assets/libs/jsontree/jsontree.min.js',
-          'src/assets/libs/typeahead/typeahead.min.js',
-          'src/assets/libs/ace/ace.min.js'
+          'node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js',
+          'node_modules/ace-builds/src-min/ace.js',
+          'node_modules/ace-builds/src-min/'
         ],
         dest: 'public/js/lib.js'
-      },
-      vendorcss: {
-        src: [
-          'src/assets/css/bootstrap.min.css',
-          'src/assets/css/font-awesome.min.css'
-        ],
-        dest: 'public/css/lib.css'
       },
       appjs: {
         src: [
@@ -53,20 +51,20 @@ module.exports = function(grunt) {
         ],
         dest: 'public/js/app.js'
       },
-      appcss: {
-        src: [
-          'src/app/css/app.css'
-        ],
-        dest: 'public/css/app.css'
-      },
+    },
+    cssmin: {
+        bootstrap: {
+            expand: false,
+            files: {'public/css/lib.css': ['src/assets/temp/bootstrap.css', 'node_modules/font-awesome/css/font-awesome.css']}
+        },
+        app: {
+            expand: false,
+            files: {'public/css/app.css': ['src/app/css/app.css']}
+        }
     },
     jshint: {
       cerebro: {
-        src: [
-          'src/app/components/*/*.js',
-          'src/app/shared/*.js',
-          'src/app/shared/*/*.js'
-        ]
+        src: ['src/app/components/*/*.js', 'src/app/shared/*.js', 'src/app/shared/*/*.js']
       }
     },
     qunit: {
@@ -75,30 +73,27 @@ module.exports = function(grunt) {
     karma: {
       unit: {configFile: 'tests/karma.config.js', keepalive: true}
     },
-    jscs: {
-      src: [
-        'src/app/app.routes.js',
-        'src/app/components/*/*.js',
-        'src/app/shared/*.js',
-        'src/app/shared/*/*.js'
-      ],
+    eslint: {
+      target: ['src/app/app.routes.js', 'src/app/components/*/*.js', 'src/app/shared/*.js', 'src/app/shared/*/*.js'],
       options: {
-        preset: 'google',
-        requireCamelCaseOrUpperCaseIdentifiers: "ignoreProperties"
-      }
+        configFile: 'conf/eslint.json',
+        failOnError: false,
+      },
     }
   });
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks("grunt-jscs");
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('dev', ['watch'])
-  grunt.registerTask('build',
-    ['clean', 'jshint', 'jscs', 'concat', 'copy', 'qunit']);
+  grunt.registerTask('build', ['clean', 'jshint', 'eslint', 'less', 'concat', 'copy', 'cssmin', 'qunit']);
   grunt.registerTask('test', ['karma'])
 };
